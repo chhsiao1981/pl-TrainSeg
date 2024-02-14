2521b3be82163cec9efe7a3deed97d26be20cde3
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-02-14 12:20:50 -0500

    Configure python-chrisapp-template/bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,10 +16,10 @@
 
 PLUGIN_NAME="$(basename $(dirname $(realpath $0)))"  # name of current directory
 PLUGIN_TITLE='My ChRIS Plugin'
-SCRIPT_NAME='commandname'
+SCRIPT_NAME='SegTrain'
 DESCRIPTION='A ChRIS plugin to do something awesome'
 ORGANIZATION='FNNDSC'
-EMAIL='dev@babyMRI.org'
+EMAIL='alan.rivasmunoz@childrens.harvard.edu'
 
 # Github Actions: automatically test and build your code.
 # https://github.com/FNNDSC/python-chrisapp-template/wiki/Continuous-Integration
@@ -46,7 +46,7 @@ ENABLE_ACTIONS_BUILD=yes
 # STEP 2. Uncomment the line where it says READY=yes
 # ----------------------------------------
 
-#READY=yes
+READY=yes
 
 # ----------------------------------------
 # STEP 3. Run: ./bootstrap.sh
